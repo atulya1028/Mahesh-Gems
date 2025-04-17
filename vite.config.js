71c4418fe396a4633a48e9ebab9cf3c6b2a3e77e
atulya1028 <atulyaagrawal1996@gmail.com>
2025-04-17 16:18:11 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For routing to work on Vercel
-  build: {
-    outDir: 'dist', // Vercel expects the build folder to be `dist`
-  },
-});
+  server: {
+    host: true,
+  }
+})
