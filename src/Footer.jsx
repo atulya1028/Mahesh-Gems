2440d153c3cc3ef296b48f5dd9003c4a6a055136
atulya1028 <atulyaagrawal1996@gmail.com>
2025-04-17 17:37:57 +0530

    Chnages in footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="mt-[50vh]">
+    <div>
       <hr className="m-10 border-t-2 border-gray-500" />
       <p className="pb-5 text-sm text-center text-gray-600">
         &copy; {new Date().getFullYear()} Mahesh Gems. All rights reserved.
